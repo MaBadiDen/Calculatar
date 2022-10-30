79de53899a1cd904843220be08c531e402fb96e0
MaBadiDen <kaba4ok.den@gmail.com>
2022-10-30 16:54:26 +0300

    ready

---
+++ src/main/java/pro/sky/Calculator/Calculator.java
@@ -0,0 +1,51 @@
+package pro.sky.Calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calculator {
+    private final CalculatorService calculatorService;
+
+    public Calculator(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping(path = "/calculator")
+    public String greetingsInCalculator() {
+        return calculatorService.greetingsInCalculator();
+    }
+    @GetMapping(path = "/calculator/plus")
+    public String plusNumbers(@RequestParam(name = "num1", required = false) Integer a , @RequestParam(name = "num2", required = false) Integer b) {
+        if(b == null || a == null) {
+            return "Не введен один или оба параметра!";
+        }
+        return calculatorService.plusNumbers(a, b);
+    }
+    @GetMapping(path = "/calculator/minus")
+    public String minusNumbers(@RequestParam(name = "num1", required = false) Integer a, @RequestParam(name = "num2", required = false) Integer b) {
+        if(b == null || a == null) {
+            return "Не введен один или оба параметра!";
+        }
+        return calculatorService.minusNumbers(a, b);
+    }
+    @GetMapping(path = "/calculator/multiply")
+    public String multiplyNumbers(@RequestParam(name = "num1", required = false) Integer a, @RequestParam(name = "num2", required = false) Integer b) {
+        if(b == null || a == null) {
+            return "Не введен один или оба параметра!";
+        }
+        return calculatorService.multiplyNumbers(a, b);
+    }
+    @GetMapping(path = "/calculator/divide")
+    public String divideNumbers(@RequestParam(name = "num1", required = false) Integer a, @RequestParam(name = "num2", required = false) Integer b) {
+        if(b == null || a == null) {
+            return "Не введен один или оба параметра!";
+        }
+        try {
+            return calculatorService.divideNumbers(a, b);
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+    }
+}
---
+++ src/main/java/pro/sky/Calculator/CalculatorService.java
@@ -0,0 +1,38 @@
+package pro.sky.Calculator;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CalculatorService {
+
+    public String greetingsInCalculator() {
+        return "Добро пожаловать в калькулятор";
+    }
+    public String plusNumbers(Integer a, Integer b) {
+        int sum = a + b;
+        return a + " + " + b + " = " + sum;
+    }
+    public String minusNumbers(Integer a, Integer b) {
+        int difference = a - b;
+        return a + " - " + b + " = " + difference;
+    }
+
+    public String multiplyNumbers(Integer a, Integer b) {
+        int product = a * b;
+        return a + " * " + b + " = " + product;
+    }
+    public String divideNumbers(Integer a, Integer b) {
+        double quotient = (double) a / (double) b;
+        if (b == 0) {
+            throw new IllegalArgumentException("На ноль делить нельзя, сволочь!");
+        }
+
+        return a + " / " + b + " = " + quotient;
+
+
+
+    }
+
+}
