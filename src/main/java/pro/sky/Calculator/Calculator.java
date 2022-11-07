611e0b64b9c9bb67c464f85a2c98ea90100130d8
MaBadiDen <kaba4ok.den@gmail.com>
2022-11-07 19:50:28 +0300

    ready

--- src/main/java/pro/sky/Calculator/Calculator.java
+++ src/main/java/pro/sky/Calculator/Calculator.java
@@ -1,10 +1,12 @@
 package pro.sky.Calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class Calculator {
     private final CalculatorService calculatorService;
 
@@ -12,40 +14,39 @@ public class Calculator {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping(path = "/calculator")
+    @GetMapping
     public String greetingsInCalculator() {
         return calculatorService.greetingsInCalculator();
     }
-    @GetMapping(path = "/calculator/plus")
+    @GetMapping(path = "/plus")
     public String plusNumbers(@RequestParam(name = "num1", required = false) Integer a , @RequestParam(name = "num2", required = false) Integer b) {
         if(b == null || a == null) {
             return "Не введен один или оба параметра!";
         }
-        return calculatorService.plusNumbers(a, b);
+        return a + " + " + b + " = " + calculatorService.plusNumbers(a, b);
     }
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "/minus")
     public String minusNumbers(@RequestParam(name = "num1", required = false) Integer a, @RequestParam(name = "num2", required = false) Integer b) {
         if(b == null || a == null) {
             return "Не введен один или оба параметра!";
         }
-        return calculatorService.minusNumbers(a, b);
+        return a + " - " + b + " = " + calculatorService.minusNumbers(a, b);
     }
-    @GetMapping(path = "/calculator/multiply")
+    @GetMapping(path = "/multiply")
     public String multiplyNumbers(@RequestParam(name = "num1", required = false) Integer a, @RequestParam(name = "num2", required = false) Integer b) {
         if(b == null || a == null) {
             return "Не введен один или оба параметра!";
         }
-        return calculatorService.multiplyNumbers(a, b);
+        return a + " * " + b + " = " + calculatorService.multiplyNumbers(a, b);
     }
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "/divide")
     public String divideNumbers(@RequestParam(name = "num1", required = false) Integer a, @RequestParam(name = "num2", required = false) Integer b) {
         if(b == null || a == null) {
             return "Не введен один или оба параметра!";
-        }
-        try {
-            return calculatorService.divideNumbers(a, b);
-        } catch (Throwable e) {
-            return e.getMessage();
+        } else if (b == 0) {
+            return "На ноль делить нельзя, сволочь!";
+        } else {
+            return a + " / " + b + " = " + calculatorService.divideNumbers(a, b);
         }
     }
 }
--- src/main/java/pro/sky/Calculator/CalculatorService.java
+++ src/main/java/pro/sky/Calculator/CalculatorService.java
@@ -10,26 +10,18 @@ public class CalculatorService {
     public String greetingsInCalculator() {
         return "Добро пожаловать в калькулятор";
     }
-    public String plusNumbers(Integer a, Integer b) {
-        int sum = a + b;
-        return a + " + " + b + " = " + sum;
+    public Integer plusNumbers(Integer a, Integer b) {
+        return a + b;
     }
-    public String minusNumbers(Integer a, Integer b) {
-        int difference = a - b;
-        return a + " - " + b + " = " + difference;
+    public Integer minusNumbers(Integer a, Integer b) {
+        return a - b;
     }
 
-    public String multiplyNumbers(Integer a, Integer b) {
-        int product = a * b;
-        return a + " * " + b + " = " + product;
+    public Integer multiplyNumbers(Integer a, Integer b) {
+        return a * b;
     }
-    public String divideNumbers(Integer a, Integer b) {
-        double quotient = (double) a / (double) b;
-        if (b == 0) {
-            throw new IllegalArgumentException("На ноль делить нельзя, сволочь!");
-        }
-
-        return a + " / " + b + " = " + quotient;
+    public double divideNumbers(Integer a, Integer b) {
+        return (double) a / (double) b;
 
 
 
